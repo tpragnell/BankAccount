0999a21c84fe2d5351fc43c3dfb4ec5cf4bdda18
Tristan Pragnell <tristan@Tristans-MBP.fios-router.home>
2020-10-27 21:12:50 -0400

    Added tests for deposit and withdraw

--- Tester.java
+++ Tester.java
@@ -8,7 +8,14 @@ public class Tester{
       System.out.println(b.getBalance());
       System.out.println(b.getAccountID());
       System.out.println(b.toString());
-      b.deposit(1000);
+
+      b.deposit(1000); // Deposit 1000 - Balance should be 1000
+      System.out.println(b.toString());
+      b.withdraw(500); // Withdraw 500 - Balance should be 500
+      System.out.println(b.toString());
+      b.deposit(-1); // Deposit -1 - Balance shouldn't change (negative)
+      System.out.println(b.toString());
+      b.withdraw(501); // Withdraw 501 - Balance shouldn't change (exceeds bal)
       System.out.println(b.toString());
     }
 }
