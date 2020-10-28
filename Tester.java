459f88dda52917a4a4c0bbcd3f82352584b94ee3
Tristan Pragnell <tristan@Tristans-MBP.fios-router.home>
2020-10-27 21:08:56 -0400

    added deposit and toString to tester

--- Tester.java
+++ Tester.java
@@ -7,5 +7,8 @@ public class Tester{
       BankAccount b = new BankAccount(5213452, "Assignment12");
       System.out.println(b.getBalance());
       System.out.println(b.getAccountID());
+      System.out.println(b.toString());
+      b.deposit(1000);
+      System.out.println(b.toString());
     }
 }
